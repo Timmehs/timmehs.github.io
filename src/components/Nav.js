8cb915b1ee87381fa50606e6db07b8279e4eac60
Tim Sandberg <tasandberg@gmail.com>
2017-09-24 14:02:05 -0700

    Add react-router, some reorganization

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => (
+  <nav>
+    <h4 className='font-effect-anaglyph brand'>Tim Sandberg</h4>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href='/blog'>Blog</a></li>
+      <li><a href='https://timsandberg.bandcamp.com/' target='_blank'>Music</a></li>
+      <li><a href='https://github.com/Timmehs' target='_blank'>Code</a></li>
+    </ul>
+  </nav>
+)
+
+export default Nav
