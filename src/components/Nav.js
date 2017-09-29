2bac152d49a3c916c74eab9d86d102c3ae76c4ea
Tim Sandberg <tasandberg@gmail.com>
2017-09-28 23:29:52 -0700

    WIP Syntax highlighting

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => (
   <nav>
-    <h4 className='font-effect-anaglyph brand'>Tim Sandberg</h4>
+    <h2 className='font-effect-anaglyph brand'>Tim Sandberg</h2>
     <ul>
       <li><NavLink exact activeClassName='navlink-active' to="/">Blog</NavLink></li>
       <li><NavLink exact activeClassName='navlink-active' to="/music">Music</NavLink></li>
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,39 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import BlogTag from './BlogTag'
 import Markdown from 'markdown-to-jsx'
 import formatDate from '../util/dateFormatter'
 
-const Post = ({ date, markdown, tags, title }) => (
-  <article>
-    <p className='right'>
-      <strong>{formatDate(date)}</strong>
-    </p>
-    <p className='right'>
-      {Object.keys(tags).map(tag => (
-        <BlogTag tagName={tag} />
-      ))}
-    </p>
-    <Markdown>
-      {markdown}
-    </Markdown>
-  </article>
-)
+class Post extends React.Component {
+  componentDidMount() {
+    const node = ReactDOM.findDOMNode(this)
+    const highlight = el => highlightBlock(el)
+
+    node.querySelectorAll('pre code').forEach(el => {
+      hljs.highlightBlock(el)
+      hljs.lineNumbersBlock(el)
+    })
+  }
+  render() {
+    const { date, markdown, tags, title } = this.props;
+
+    return (
+      <article>
+        <p className='right'>
+          <strong>{formatDate(date)}</strong>
+        </p>
+        <p className='right'>
+          {Object.keys(tags).map(tag => (
+            <BlogTag tagName={tag} key={tag} />
+          ))}
+        </p>
+        <Markdown>
+          {markdown}
+        </Markdown>
+      </article>
+    )
+  }
+}
+
 
 export default Post
