ab1841aff72b4f360a318f88975b97713ff4b0d8
Tim Sandberg <tasandberg@gmail.com>
2017-10-03 10:57:28 -0700

    Header style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => (
   <nav>
-    <h2 className='font-effect-anaglyph brand'>Tim Sandberg</h2>
+    <h3 className='font-effect-anaglyph brand'>Tim<br/>Sandberg</h3>
     <ul>
       <li><NavLink exact activeClassName='navlink-active' to="/">Blog</NavLink></li>
       <li><NavLink exact activeClassName='navlink-active' to="/music">Music</NavLink></li>
--- src/components/Post.js
+++ src/components/Post.js
@@ -10,7 +10,6 @@ class Post extends React.Component {
     const highlight = el => highlightBlock(el)
 
     node.querySelectorAll('pre').forEach(el => {
-      console.log(el)
       hljs.highlightBlock(el)
       hljs.lineNumbersBlock(el)
     })
@@ -19,15 +18,17 @@ class Post extends React.Component {
     const { date, markdown, tags, title } = this.props;
 
     return (
-      <article>
-        <p className='right'>
-          <strong>{formatDate(date)}</strong>
-        </p>
-        <p className='right'>
+      <article className='post-body'>
+        <header className='left'>
+          <h2 className='post-body-title'>{title}</h2>
+          <strong className='post-date'>{formatDate(date)}</strong>
+        </header>
+        <p className='post-tags'>
           {Object.keys(tags).map(tag => (
             <BlogTag tagName={tag} key={tag} />
           ))}
         </p>
+        <div className='post-header-image' />
         <Markdown>
           {markdown}
         </Markdown>
