9ccfd0e42bf526789c68bfb04d7a0bd54bc04174
Tim Sandberg <tasandberg@gmail.com>
2019-04-14 15:00:28 -0700

    4-14-2019 post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import BlogTag from './BlogTag'
-import Markdown from 'markdown-to-jsx'
-import formatDate from '../util/dateFormatter'
+import React from "react";
+import ReactDOM from "react-dom";
+import BlogTag from "./BlogTag";
+import Markdown from "markdown-to-jsx";
+import formatDate from "../util/dateFormatter";
 
 class Post extends React.Component {
   componentDidMount() {
-    const node = ReactDOM.findDOMNode(this)
-    const highlight = el => highlightBlock(el)
+    const node = ReactDOM.findDOMNode(this);
+    const highlight = el => highlightBlock(el);
 
-    node.querySelectorAll('pre').forEach(el => {
-      hljs.highlightBlock(el)
-      hljs.lineNumbersBlock(el, { singleLine: true })
-    })
+    node.querySelectorAll("pre").forEach(el => {
+      hljs.highlightBlock(el);
+      hljs.lineNumbersBlock(el, { singleLine: true });
+    });
   }
   render() {
     const { post } = this.props;
     const headerImage = {
-      backgroundImage: `url(${post.get('header')})`
-    }
+      backgroundImage: `url(${post.get("header")})`
+    };
     return (
-      <article className='post-body'>
-        <header className='left'>
-          <h2 className='post-body-title'>{post.get('title')}</h2>
-          <strong className='post-date'>{formatDate(post.get('date'))}</strong>
+      <article className="post-body">
+        <header className="left">
+          <h1 className="post-body-title">{post.get("title")}</h1>
+          <strong className="post-date">{formatDate(post.get("date"))}</strong>
         </header>
-        <p className='post-tags'>
-          {post.get('tags').keySeq().map(tag => (
-            <BlogTag tagName={tag} key={tag} />
-          ))}
+        <p className="post-tags">
+          {post
+            .get("tags")
+            .keySeq()
+            .map(tag => (
+              <BlogTag tagName={tag} key={tag} />
+            ))}
         </p>
-        <div className='post-header-image' style={headerImage} />
-        <Markdown>
-          {post.get('markdown')}
-        </Markdown>
+        <div className="post-header-image" style={headerImage} />
+        <Markdown>{post.get("markdown")}</Markdown>
       </article>
-    )
+    );
   }
 }
 
-
-export default Post
+export default Post;
