518574547c9092e8d93cf4199015f8985b36a6b4
Tim Sandberg <tasandberg@gmail.com>
2019-04-14 15:19:42 -0700

    Update deps

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Nav from './Nav'
-import Blog from './Blog'
-import Links from './Links'
-import Music from './Music'
+import Nav from "./Nav";
+import Blog from "./Blog";
+import Links from "./Links";
+import Music from "./Music";
 
 const Main = () => (
-  <Router >
-    <div className='row'>
-      <div className='col-md-2 col-xs-12'>
+  <Router>
+    <div className="row">
+      <div className="col-md-2 col-xs-12">
         <Nav />
       </div>
-      <div className='col-md-8 col-xs-12 main-content'>
+      <div className="col-md-8 col-xs-12 main-content">
         <Route exact path="/" component={Blog} />
         <Route path="/music" component={Music} />
         <Route path="/links" component={Links} />
       </div>
     </div>
   </Router>
-)
+);
 
-export default Main
+export default Main;
