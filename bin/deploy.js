85052e265730e637f81336240c4de7bcfca1911c
Tim Sandberg <tasandberg@gmail.com>
2019-04-30 22:28:22 -0700

    Fixes

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,5 +1,11 @@
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
-ghpages.publish('build', {
-  branch: 'master'
-})
+ghpages.publish(
+  "build",
+  {
+    branch: "master"
+  },
+  function() {
+    console.log("Deploy successful");
+  }
+);
