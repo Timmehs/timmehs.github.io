829470e24763e42a75d2e0fc4f127bb55e4be9ce
Tim Sandberg <tasandberg@gmail.com>
2019-04-30 21:54:25 -0700

    Pre-clean test

--- webpack.common.js
+++ webpack.common.js
@@ -1,64 +1,66 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const htmlTemplateFilename = 'html-template.ejs'
+const htmlTemplateFilename = "html-template.ejs";
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, 'src', 'index.js'),
-    blog: path.resolve(__dirname, 'src', 'store.js')
+    main: path.resolve(__dirname, "src", "index.js"),
+    blog: path.resolve(__dirname, "src", "store.js")
   },
   output: {
-    filename: 'assets/[name].js',
-    chunkFilename: 'assets/[name].bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
+    filename: "assets/[name].js",
+    chunkFilename: "assets/[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/"
   },
   optimization: {
     splitChunks: {
-      chunks: 'all'
+      chunks: "all"
     }
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["!CNAME", "!favicon.ico", "!README.md"]
+    }),
     new webpack.ContextReplacementPlugin(
       /highlight\.js\/lib\/languages$/,
       new RegExp(`^./(ruby|javascript|css|scss|bash)`)
     ),
     new HtmlWebpackPlugin({
-      title: require('./package.json').name,
-      template: path.resolve('src', htmlTemplateFilename),
+      title: require("./package.json").name,
+      template: path.resolve("src", htmlTemplateFilename),
       inject: false
     })
   ],
   resolveLoader: {
-    modules: ['node_modules', './node_modules/buddhablog-cli/lib/webpack']
+    modules: ["node_modules", "./node_modules/buddhablog-cli/lib/webpack"]
   },
   module: {
     rules: [
       {
         test: /\.md$/,
-        include: [path.resolve(__dirname, 'posts')],
-        use: ['buddha-post-loader']
+        include: [path.resolve(__dirname, "posts")],
+        use: ["buddha-post-loader"]
       },
       {
         test: /\.md$/,
-        include: [path.resolve(__dirname, 'pages')],
-        use: ['buddha-page-loader']
+        include: [path.resolve(__dirname, "pages")],
+        use: ["buddha-page-loader"]
       },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: require.resolve('babel-loader'),
+          loader: require.resolve("babel-loader"),
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: [
-              'babel-plugin-import-glob',
-              '@babel/plugin-proposal-object-rest-spread',
-              'react-hot-loader/babel'
+              "babel-plugin-import-glob",
+              "@babel/plugin-proposal-object-rest-spread",
+              "react-hot-loader/babel"
             ]
           }
         }
@@ -66,11 +68,11 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          { loader: 'style-loader' }, // creates style nodes from JS strings
-          { loader: 'css-loader' }, // translates CSS into CommonJS
-          { loader: 'sass-loader' } // compiles Sass to CSS
+          { loader: "style-loader" }, // creates style nodes from JS strings
+          { loader: "css-loader" }, // translates CSS into CommonJS
+          { loader: "sass-loader" } // compiles Sass to CSS
         ]
       }
     ]
   }
-}
+};
